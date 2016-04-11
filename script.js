d0264cbb8e44dd743826eda0b3e5b17abdc59408
Charles <GUITARPLRC@YAHOO.COM>
2016-04-10 23:53:01 -0600

    started html and css

---
+++ script.js
@@ -0,0 +1,18 @@
+var link1 = document.getElementById("link1");
+var link2 = document.getElementById("link2");
+var link3 = document.getElementById("link3");
+var list = document.getElementById("list");
+
+link1.click();
+link2.click();
+link3.click();
+
+function  (){
+	
+}
+function  (){
+	
+}
+function  (){
+	
+}
