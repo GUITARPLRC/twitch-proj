0979723ba8a865652f0c4c6ee76b73bee77cfac2
Charles <GUITARPLRC@YAHOO.COM>
2016-04-11 23:01:13 -0600

    start twitch api

--- script.js
+++ script.js
@@ -2,17 +2,18 @@ var link1 = document.getElementById("link1");
 var link2 = document.getElementById("link2");
 var link3 = document.getElementById("link3");
 var list = document.getElementById("list");
-
+/*
 link1.click();
 link2.click();
 link3.click();
-
-function  (){
-	
-}
+*/
+getTwitchers();
 function getTwitchers (){
 	
-}
-function  (){
+	$.getJSON("https://api.twitch.tv/kraken/streams/followed",function (data){
+		
+		console.log(data);
+		
+	})
 	
 }
