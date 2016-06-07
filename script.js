51a5dc8e33c017249214b452d3d05957c705f550
Charles <GUITARPLRC@YAHOO.COM>
2016-06-07 16:05:41 -0500

    changed link style, trying to add live stream check

--- script.js
+++ script.js
@@ -1,47 +1,6 @@
-var all = document.getElementById("all");
-var online = document.getElementById("online");
-var offline = document.getElementById("offline");
 var submit = document.getElementById("submit");
 
 submit.addEventListener("click", getUser, false);
-/*
-all.addEventListener("", ,false);
-online.addEventListener("", ,false);
-offline.addEventListener("", ,false);
-*/
-
-function getTwitchers (user){
-	
-	$.getJSON("https://api.twitch.tv/kraken/users/"+ user +"/follows/channels", function (data){
-		
-		var list = document.getElementById("list");
-		
-		for (var i =0; i < data.follows.length; i++) {
-		
-			var anchor = document.createElement("a");
-				anchor.setAttribute("href", data.follows[i].channel.url); // set streamer link to
-			var item = document.createElement("li");
-				item.setAttribute("class", "streamer");
-			var div = document.createElement("div");
-			var pic = document.createElement("img");
-				pic.setAttribute("src", data.follows[i].channel.logo); // set streamer logo
-				pic.setAttribute("class", "picture");
-			var header = document.createElement("h3");
-				header.textContent = data.follows[i].channel.display_name; // set streamer name
-			var description = document.createTextNode(data.follows[i].channel.status); // set streamer status (if "live")
-			
-			div.appendChild(pic); // div > pic
-			div.appendChild(header); // div > pic > header
-			div.appendChild(description); // div > pic > header > description
-			item.appendChild(div); // li(item) > div...
-			anchor.appendChild(item); // a > li(item) > div...
-			list.appendChild(anchor); // list > a > li(item) > div > pic > header > description
-		
-		}
-		
-	});
-	
-}
 
 function getUser() {
 	
@@ -51,6 +10,41 @@ function getUser() {
 	
 	var user = document.getElementById("user").value;
 	
-	getTwitchers(user);
+	getChannels(user);
+	
+}
+
+function getChannels(user){
+	
+	$.getJSON("https://api.twitch.tv/kraken/users/"+ user +"/follows/channels", function (data){
+		
+		var list = document.getElementById("list");
+		
+		for (var i =0; i < data.follows.length; i++) {
+      
+      var anchor = document.createElement("a");
+				anchor.setAttribute("href", data.follows[i].channel.url); // set channel link to
+			var item = document.createElement("li");
+				item.setAttribute("class", "streamer");
+			var div = document.createElement("div");
+			var pic = document.createElement("img");
+				pic.setAttribute("src", data.follows[i].channel.logo); // set channel logo
+				pic.setAttribute("class", "picture");
+			var header = document.createElement("h3");
+				header.textContent = data.follows[i].channel.display_name; // set channel name
+        
+			var description = document.createTextNode(data.follows[i].channel.status); // set channel status (if "live")
+			
+			div.appendChild(pic); // div > pic
+			div.appendChild(header); // div > pic > header
+			div.appendChild(description); // div > pic > header > description
+      
+			item.appendChild(anchor); // li(item) > a...
+			anchor.appendChild(div); // li(item) > a > div...
+			list.appendChild(item); // list > li(item) > a > div > pic > header > description
+		
+		}
+		
+	});
 	
 }
